9898c9126a9f3fac3b5549c0a086e6a2b4913e02
Ashraful Haque Piash <piashrafulhaque@gmail.com>
2024-08-17 23:11:53 +0600

    Initial commit

---
+++ newbeginning/src/main/java/com/piashraful/newbeginning/NewbeginningApplication.java
@@ -0,0 +1,13 @@
+package com.piashraful.newbeginning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbeginningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbeginningApplication.class, args);
+	}
+
+}
---
+++ newbeginning/src/test/java/com/piashraful/newbeginning/NewbeginningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piashraful.newbeginning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbeginningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
